b3fb9616bed122504646083f36d5dded1d05ec30
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-07-28 00:45:04 +0200

    Avance 5 - proyecto - configuracion cookies

--- MASTER_DATA_ANALYST/Proyecto1/script.js
+++ MASTER_DATA_ANALYST/Proyecto1/script.js
@@ -36,3 +36,20 @@ function seleccionar(){
     window.onscroll = function(){
         efectoSkills();
     }
+
+//funciones del aviso-cookies
+const botonAceptarCookies = document.getElementById("boton-aceptar-cookies");
+const avisoCookies = document.getElementById("aviso-cookies");
+const fondoAvisoCookies = document.getElementById("fonde-aviso-cookies");
+
+if(!localStorage.getItem('cookies-aceptadas')){
+    avisoCookies.classList.add('activo');
+    fondoAvisoCookies.classList.add('activo');
+}
+
+botonAceptarCookies.addEventListener("click", ()=>{
+    avisoCookies.classList.remove('activo');
+    fondoAvisoCookies.classList.remove('activo');
+
+    localStorage.setItem('cookies-aceptadas', true);
+});
