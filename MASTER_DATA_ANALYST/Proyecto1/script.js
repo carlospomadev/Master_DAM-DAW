48b111b24041d93c21dfd3e76dea00c8c894346b
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-07-28 23:50:59 +0200

    Avance corrección de datos y adición de tipografías locales

--- MASTER_DATA_ANALYST/Proyecto1/script.js
+++ MASTER_DATA_ANALYST/Proyecto1/script.js
@@ -23,9 +23,9 @@ function seleccionar(){
             let habilidades = document.getElementsByClassName("progreso");
             habilidades[0].classList.add("javascript");
             habilidades[1].classList.add("htmlcss");
-            habilidades[2].classList.add("photoshop");
-            habilidades[3].classList.add("wordpress");
-            habilidades[4].classList.add("drupal");
+            habilidades[2].classList.add("Java");
+            habilidades[3].classList.add("python");
+            habilidades[4].classList.add("sql");
             habilidades[5].classList.add("comunicacion");
             habilidades[6].classList.add("trabajoenequipo");
             habilidades[7].classList.add("creatividad");
